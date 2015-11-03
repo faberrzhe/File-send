a4b27aa9b50747cb802235290c752a9480c9e862
faberrzhe <faberrzhe@gmail.com>
2015-11-03 13:26:42 +0300

    Bug fix for previos commit

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 import socket, threading, re, sys, os, time, random, getopt
 class Init():
     def __init__(self):
+        print('Hello!')
     def run(self):
         self.parse_config()
         self.parse_sys()
@@ -10,24 +12,25 @@ class Init():
         global server_id, interface,ident_server, hello_interval, client_threads, fragment_size
         variable_list={}
         try:
-            config_file=open(config_file_name,'r')
+            config_file=open(config_file_name,'r+',encoding='utf-8')
         except OSError:
             answer=input('No config file. Create default(YES)?')
-            if answer=='Yes' or answer='':
+            if answer=='Yes' or answer=='':
                 config_file=open(config_file_name,'w')
                 random.seed()
                 server_id='00000000000'+str(random.randint(1,9999999999))
                 server_id=server_id[-10:]
                 default_config='server_id='+server_id+'\r\ninterface=single\r\nident_server=none\r\nhello_interval=60\r\nclient_threads\r\nfragment_size=2000000'
+                config_file.write(default_config)
             else:
                 sys.exit(1)
-        for current_line in config_file.readline():
+        for line_ in config_file:
             try:
-                variable_name=re.findall('(&!#)(\S+)=',current_line)
+                variable_name=re.findall('(?!#)(\S+)=\S+',line_)[0]
             except IndexError:
                 pass
             else:
-                variable_value=re.findall('=(\S+)',current_line)
+                variable_value=re.findall('=(\S+)[\\n|\\r\\n]',line_)[0]
                 variable_list[variable_name]=variable_value
         for key in variable_list:
             if key=='server_id':
@@ -59,5 +62,5 @@ class Init():
 
 if __name__=="__main__":
     config_file_name='config.txt'
-    Proggramm=init()
-    Proggramm.start()
+    Proggramm=Init()
+    Proggramm.run()

ff936ca3c0f2f846b2c09a5d54749848d4a27d9f
eberezhnoy <faberrzhe@gmail.com>
2015-11-05 18:00:30 +0300

    Bugfix, wrote parse index

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-import socket, threading, re, sys, os, time, hashlib, shutil
+import socket, threading, re, sys, os, hashlib, shutil, queue
 class Init():
     def __init__(self):
         print('Hello!')
@@ -82,34 +82,80 @@ class Init():
             indexfile.write(bytes(index,'utf-8'))
             indexfile.close()
         parentfile.close()
+
+class fragment_send(threading.Thread):
+    def __init__(self):
+        self.run()
+    def run(self):
+        while True:
+            try:
+                filename=queue_.get_nowait()
+            except:
+                return
+            global server_ip,queue_
+            new_socket=socket.socket()
+            new_socket.connect((server_ip,5666))
+            short_send_filename=re.findall('\S+\/(\S+)',filename)[0]
+            file_size=str(os.path.getsize(filename))
+            file=open(filename,'rb')
+            new_socket.send(bytes('FRAGMENT::','utf-8'))
+            new_socket.send(bytes(short_send_filename+'\r\n','utf-8'))
+            new_socket.send(bytes(file_size+'\r\n','utf-8'))
+            data=file.read(1500)
+            while data:
+                new_socket.send(data)
+                data=file.read(1500)
+            try:
+                receive=str(new_socket.recv(5),'utf-8')
+            except socket.timeout:
+                queue_.put(filename)
+            if receive!='ACK::':
+                queue_.put(filename)
+            file.close()
+            new_socket.close()
+
 class Client():
     def __init__(self):
         print('Initiating client')
         self.run()
     def run(self):
         print('Starting client')
-        global server_ip, work_directory, short_send_filename
+        global server_ip, work_directory, short_send_filename,queue_
+        threads=10
+        queue_=queue.Queue()
         main_socket=socket.socket()
         main_socket.connect((server_ip,5666))
         main_socket.send(bytes('INDEX::','utf-8'))
         indexfilename=work_directory+short_send_filename+'.index'
+        indexfile_short_name=re.findall('\S+\/(\S+)',indexfilename)[0]
         index_file_size=str(os.path.getsize(indexfilename))
         indexfile=open(indexfilename,'rb')
-        main_socket.send(bytes(short_send_filename+'.index\r\n','utf-8'))
+        main_socket.send(bytes(indexfile_short_name+'\r\n','utf-8'))
         main_socket.send(bytes(index_file_size+'\r\n','utf-8'))
         data=indexfile.read(1500)
         while data:
             main_socket.send(data)
             data=indexfile.read(1500)
-        receive=str(main_socket.recv(1))
-        while receive[-2:]!='::' or len(receive)<50:
-            receive+=str(main_socket.recv(1))
+        indexfile.close()
+        receive=str(main_socket.recv(1),'utf-8')
+        while receive[-2:]!='::' and len(receive)<50:
+            receive+=str(main_socket.recv(1),'utf-8')
         if receive=='GET_FRAGMENTS::':
-            receive=str(main_socket.recv(1))
-            while receive[-2:]!='::' or len(receive)<50:
-                receive+=str(main_socket.recv(1))
+            print(receive)
+            receive=str(main_socket.recv(1),'utf-8')
+            while receive[-2:]!='::':
+                receive+=str(main_socket.recv(1),'utf-8')
             fragmentlist=receive[:-2].split(',')
             print(fragmentlist)
+            for i in fragmentlist:
+                fragment_name=work_directory+short_send_filename+'/'+short_send_filename+'_part'+str(i)
+                queue_.put(fragment_name)
+            i=0
+            for i in range (0,threads):
+                fragment_send()
+        elif receive=='DONE::':
+            print('Send sucessful!')
+            main_socket.close()
         else:
             print('GET_FRAGMENTS:: expected, but received'+receive)
 
--- server.py
+++ server.py
@@ -1,80 +1,100 @@
 #!/usr/bin/env python3
-import socket, threading, re, sys, os, time
-import socket
-import os
-import time
-import threading
-import select
-threads=10
-sock=socket.socket()
-sock.bind(('',5666))
-sock.listen(10)
-inputs=[sock]
-th=[]
-while inputs:
-    r,w,e=select.select(inputs,[],[])
-    for j in r:
-        conn,addr=j.accept()
-        flag=str(conn.recv(1),'utf-8')
-        while flag[-2:]!='::' or len(flag)<50:
-            flag+=str(conn.recv(1),'utf-8')
-        if flag == 'INDEX::':
-            th=server(conn)
-            result=th.run()
-            print(result)
-            if result==0:
-                conn.send(bytes('GET_FRAGMENTS::2,4,6,8,77,888::'))
-        elif flag== 'FRAGMENT':
-            print('Fragment')
-        else:
-            print('Error')
-            conn.close()
-class server(threading.Thread):
+import socket, threading, re, sys, os, time, select, hashlib
+class file_receive(threading.Thread):
     def __init__(self,conn):
         threading.Thread.__init__(self)
         self.conn=conn
         print(conn)
     def run(self):
+        global work_directory
         while True:
-            #conn,addr=s.accept()
-            data=str(self.conn.resv(1),'utf-8')
+            data=str(self.conn.recv(1),'utf-8')
             while data[-2:]!='\r\n':
-                data+=str(self.conn.resv(1),'utf-8')
+                data+=str(self.conn.recv(1),'utf-8')
             filename = data[:-2]
-            data=str(self.conn.resv(1),'utf-8')
+            data=str(self.conn.recv(1),'utf-8')
             while data[-2:]!='\r\n':
-                data+=str(self.conn.resv(1),'utf-8')
+                data+=str(self.conn.recv(1),'utf-8')
             filesize = int(data[:-2])
-            startfilesize=filesize
-            file = open('/home/azureuser'+filename, 'wb')
-            print('Receving file',filename,'(',filesize, 'bytes) from ',addr, ' in thread ',self.number)
-            start_time=time.time()
+            file = open(work_directory+filename, 'wb')
             while filesize>0:
                 if filesize>4096:
                     data = self.conn.recv(4096)
-                    #print(str(filesize),str(data))
                     datasize=len(data)
                     filesize=filesize-datasize
                     file.write(data)
-                    #if (filesize%50000000)<4000:
-                    #        print(str(round(filesize/1048576)),' MB Left to recieve')
                 else:
                     data = self.conn.recv(filesize)
                     datasize=len(data)
                     filesize=filesize-datasize
                     file.write(data)
-                    #print(str(filesize),str(data))
             file.close()
-            end_time=time.time()
-            minutes,seconds=divmod(end_time-start_time,60)
-            speed=startfilesize/(end_time-start_time)
-            if speed>500000:
-                speed=str(round(speed/1048576,2))+' MB/s'
-            else:
-                speed=str(round(speed/1024,2))+' KB/s'
-            print('Received file ',filename,' in ',int(minutes),'min',int(seconds),'sec. Speed: ',speed,' from ',addr, ' in thread ',self.number)
-            #self.conn.close()
-            return 0
+            return self.conn,filename
 
-#if __name__=="__main__":
-#todo
+def Parse_index(indexfilename):
+    global work_directory
+    with open(work_directory+indexfilename,'rb') as indexfile:
+        need_fragments=[]
+        filename=work_directory+str(indexfile.readline()[:-2],'utf-8')
+        fragmentsize=int(indexfile.readline()[:-2])
+        line=str(indexfile.readline()[:-2],'utf-8')
+        while line:
+            fragmentnumber,index_hashsumd=line.split(" ")
+            try:
+                file=open(filename+'_part'+fragmentnumber,'rb')
+            except FileNotFoundError:
+                need_fragments.append(fragmentnumber)
+            else:
+                fileread=file.read(fragmentsize)
+                file_hashsumd=hashlib.md5(fileread).hexdigest()
+                #file_hashsumd=file_hashsum.hexdigits()
+                if index_hashsumd==file_hashsumd:
+                    file.close()
+                else:
+                    file.close()
+                    os.remove(filename+'_part'+fragmentnumber)
+                    need_fragments.append(fragmentnumber)
+            line=str(indexfile.readline()[:-2],'utf-8')
+    need_fragments=','.join(need_fragments)
+    return need_fragments
+
+class server():
+    def __init__(self):
+        self.run()
+    def run(self):
+        global work_directory
+        work_directory='C:/Users/eberezhnoy/python/'
+        sock=socket.socket()
+        sock.bind(('',5666))
+        sock.listen(50)
+        inputs=[sock]
+        while inputs:
+            r,w,e=select.select(inputs,[],[])
+            for current_socket in r:
+                conn,addr=current_socket.accept()
+                flag=str(conn.recv(1),'utf-8')
+                while flag[-2:]!='::' and len(flag)<50:
+                    flag+=str(conn.recv(1),'utf-8')
+                if flag == 'INDEX::':
+                    th=file_receive(conn)
+                    returned_connection,indexfilename=th.run()
+                    print(returned_connection,indexfilename)
+                    need_fragments=Parse_index(indexfilename)
+                    print(need_fragments)
+                    if need_fragments='':
+                        returned_connection.send(bytes('DONE::','utf-8'))
+                    else:
+                        returned_connection.send(bytes('GET_FRAGMENTS::' + need_fragments+'::','utf-8'))
+                elif flag== 'FRAGMENT::':
+                    th=file_receive(conn)
+                    returned_connection,indexfilename=th.run()
+                    print(returned_connection,indexfilename)
+                    returned_connection.send(bytes('ACK::','utf-8'))
+                    returned_connection.close()
+                else:
+                    print('Error')
+                    conn.close()
+
+
+if __name__=="__main__":
+    server()
